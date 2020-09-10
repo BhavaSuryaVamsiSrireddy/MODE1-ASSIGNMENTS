63c67bd62ff98159682a69d4b4aef0e5b74e2f07
BhavaSuryaVamsiSrireddy <43814473+BhavaSuryaVamsiSrireddy@users.noreply.github.com>
2020-09-10 22:03:56 +0530

    Update Sorting.java

--- Day3/src/com/logic/Sorting.java
+++ Day3/src/com/logic/Sorting.java
@@ -8,10 +8,10 @@ public class Sorting {
 		
 		for (int i = 0; i < numbers.length - 1; i++) {
 			for (int j = 0; j < numbers.length - i - 1; j++) {
-				if(numbers[i] > numbers[j]) {
-					temp = numbers[i];
-					numbers[i] = numbers[j];
-					numbers[j] = temp;
+				if(numbers[j] > numbers[j+1]) {
+					temp = numbers[j];
+					numbers[j] = numbers[j+1];
+					numbers[j+1] = temp;
 				}
 			}
 		}
